b3a98bb18aa650e93be5ce0eecd56d7747963e90
singhtanveer38 <singhtanveer38@yahoo.com>
2022-06-16 20:01:46 +0530

    requirement added

--- src/credentials.py
+++ src/credentials.py
@@ -2,12 +2,12 @@
 api_key = "f6c07cdf904927f00d0887826b8181ed"
 
 # location
-latitude = "" 
-longitude = ""
+latitude = "27.4886" 
+longitude = "95.3558"
 
 # snowflake
-user=""
-password=""
-account=""
-db = ""
-wh = ""
+user='tanveers'
+password='Thisissnowflakepassword@123'
+account='nr11344.ap-south-1.aws'
+db = "weather_station"
+wh = "weather_station_wh"
--- src/fetch.py
+++ src/fetch.py
@@ -1,11 +1,6 @@
 import credentials
 import requests
-# from datetime import datetime
-# import pprint
-
 
 def fetch():
     temp = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={credentials.latitude}&lon={credentials.longitude}&appid={credentials.api_key}&units=metric").json()
-    # pprint.pprint(temp)
     return temp
-
